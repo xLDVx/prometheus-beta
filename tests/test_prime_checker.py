d000fda87ffd027aff3c7365eb477b1edf1ab366
xLDVx <xLDVx@users.noreply.github.com>
2025-03-08 15:37:09 +0000

    Fix test for non-prime numbers removing 1 from test cases

--- tests/test_prime_checker.py
+++ tests/test_prime_checker.py
@@ -9,7 +9,7 @@ def test_prime_numbers():
 
 def test_non_prime_numbers():
     """Test known non-prime numbers"""
-    non_prime_numbers = [1, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30, 999]
+    non_prime_numbers = [4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30, 999]
     for num in non_prime_numbers:
         assert is_prime(num) is False, f"{num} should not be prime"
 
